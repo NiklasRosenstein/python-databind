ab09bdc2afeb88b2cd4e82785683081bac3c9c5d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-08-08 15:34:06 +0200

    databind.json: implement array and model converters

--- databind.json/src/databind/json/tests/test_json.py
+++ databind.json/src/databind/json/tests/test_json.py
@@ -1,7 +1,7 @@
 
 import decimal
 import enum
-from typing import Optional, Union
+from typing import Dict, List, Optional, Union
 from databind.core import *
 from databind.json import *
 from pytest import raises
@@ -88,3 +88,55 @@ def test_mixtype_converter():
   with raises(ConversionTypeError) as excinfo:
     from_json(Union[int, str], 342.324)
   assert str(excinfo.value) == "$: expected typing.Union[int, str], got float"
+
+
+def test_array_converter():
+  assert from_json(List[int], [1, 2, 3]) == [1, 2, 3]
+  assert to_json([1, 2, 3], List[int]) == [1, 2, 3]
+
+  @datamodel
+  class A:
+    a: str
+
+  assert from_json(List[A], [{'a': 'foo'}, {'a': 'bar'}]) == [A('foo'), A('bar')]
+  assert to_json([A('foo'), A('bar')], List[A]) == [{'a': 'foo'}, {'a': 'bar'}]
+
+
+def test_model_converter_flatten():
+  @datamodel
+  class A:
+    a: str
+
+  @datamodel
+  class B:
+    a: A = field(flatten=True)
+    b: str
+
+  assert from_json(B, {'a': 'foo', 'b': 'bar'}) == B(A('foo'), 'bar')
+  #assert to_json(B(A('foo'), 'bar')) == {'a': 'foo', 'b': 'bar'}
+
+
+def test_model_converter_flatten_wildcard():
+  @datamodel
+  class A:
+    a: str
+
+  @datamodel
+  class C:
+    a: int
+    remainder: Dict[str, A] = field(flatten=True)
+
+  assert from_json(C, {'a': 42, 'b': {'a': 'foo'}, 'c': {'a': 'bar'}}) == C(42, {'b': A('foo'), 'c': A('bar')})
+  assert to_json(C(42, {'b': A('foo'), 'c': A('bar')})) == {'a': 42, 'b': {'a': 'foo'}, 'c': {'a': 'bar'}}
+
+
+def test_model_converter_strict():
+  @datamodel
+  class A:
+    a: str
+
+  assert from_json(A, {'a': 'foo'}) == A('foo')
+  assert from_json(A, {'a': 'foo', 'b': 'bar'}) == A('foo')
+  with raises(ConversionValueError) as excinfo:
+    from_json(A, {'a': 'foo', 'b': 'bar'}, field_metadata=FieldMetadata(strict=True))
+  assert str(excinfo.value) == f"$: strict conversion of {type_repr(A)} does not permit additional keys {{'b'}}"
