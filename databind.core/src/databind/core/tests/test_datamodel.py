325e01c3aa08aa23129dac08a99a3dbf74af5eeb
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-08-08 12:02:05 +0200

    allow non-default arguments after default arguments in @datamodel() classes

--- databind.core/src/databind/core/tests/test_datamodel.py
+++ databind.core/src/databind/core/tests/test_datamodel.py
@@ -1,6 +1,7 @@
 
 from databind.core._datamodel import *
 from databind.core._union import StaticUnionResolver
+from pytest import raises
 
 
 def test_uniontype_decorator():
@@ -31,12 +32,37 @@ def test_datamodel_decorator():
 
   @datamodel(kwonly=True)
   class B:
-    pass
+    field: int
 
   assert hasattr(B, '__databind_metadata__')
   assert isinstance(B.__databind_metadata__, ModelMetadata)
   assert B.__databind_metadata__.kwonly == True
   assert ModelMetadata.for_type(B) == ModelMetadata(kwonly=True)
+
+  with raises(TypeError) as excinfo:
+    B(42)
+  assert str(excinfo.value) == '__init__() takes 1 positional argument but 2 were given'
+
+  assert B(field=42).field == 42
+
+
+def test_datamodel_mixed_order_default_arguments():
+  """
+  The #@datamodel() decorator overrides the behavior of the underlying #@dataclass() decorator
+  and allows for non-default arguments to follow default arguments.
+  """
+
+  @datamodel
+  class A:
+    a: int = field(default=0)
+    b: int
+
+  assert A(b=10) == A(0, 10)
+  assert A(b=10, a=20) == A(20, 10)
+
+  with raises(TypeError) as excinfo:
+    A(42)
+  assert str(excinfo.value) == "missing required argument 'b'"
 
 
 def test_FieldMetadata_constructor():
