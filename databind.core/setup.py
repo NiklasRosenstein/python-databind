445ae60ba7e14bbc4bf87e358aed5dd0b2aedbbb
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-08-15 01:00:52 +0200

    (databind.core) bump version to 0.1.1.post1

--- databind.core/setup.py
+++ databind.core/setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setuptools.setup(
   name = 'databind.core',
-  version = '0.1.1',
+  version = '0.1.1.post1',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'Databind is a library inspired by Jackson-databind to describe and bind data models for object-oriented programming.',
