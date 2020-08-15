e4517357f822566ea6b717f03d51b2aa35230326
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-08-15 15:42:20 +0200

    (databind.yaml) bump version to 0.1.0

--- databind.yaml/src/databind/yaml/__init__.py
+++ databind.yaml/src/databind/yaml/__init__.py
@@ -1,6 +1,6 @@
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 
 from typing import Any, Dict, Optional, TextIO, Type, TypeVar, Union
 from databind import json as _json
