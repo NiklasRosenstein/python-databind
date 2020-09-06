bf6fef7d5cce17fce9f78ff0e1de4fd48f3a10ed
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-09-06 23:27:03 +0200

    (databind.json) bump version to 0.5.0

--- databind.json/setup.py
+++ databind.json/setup.py
@@ -22,7 +22,7 @@ requirements = [
 
 setuptools.setup(
   name = 'databind.json',
-  version = '0.4.1',
+  version = '0.5.0',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'Bind object-oriented data models to JSON data.',
