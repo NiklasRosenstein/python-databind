8cf39cb8cb27ef0a37c3008dbbc9fe3380fa4239
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-09-06 12:35:38 +0200

    (databind.json) bump version to 0.4.1

--- databind.json/setup.py
+++ databind.json/setup.py
@@ -15,11 +15,14 @@ else:
   print("warning: file \"{}\" does not exist.".format(readme_file), file=sys.stderr)
   long_description = None
 
-requirements = []
+requirements = [
+  'databind.core >=0.4.0,<1.0.0',
+  'nr.parsing.date >=0.3.0,<1.0.0',
+]
 
 setuptools.setup(
   name = 'databind.json',
-  version = '0.4.0',
+  version = '0.4.1',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'Bind object-oriented data models to JSON data.',
