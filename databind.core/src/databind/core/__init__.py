a387643441cc6fa98a78434dde6156a0cd092eda
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-09-09 17:42:35 +0200

    (databind.core) bump version to 0.4.2

--- databind.core/src/databind/core/__init__.py
+++ databind.core/src/databind/core/__init__.py
@@ -5,7 +5,7 @@
 #![feature(member_annotations)]
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 from ._converter import *
 from ._datamodel import *
