ddaec881fda71f8bdafbdc02019dde5598ab19fb
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-11-24 02:34:48 +0100

    (databind.json) bump version to 0.8.0

--- databind.json/src/databind/json/__init__.py
+++ databind.json/src/databind/json/__init__.py
@@ -1,6 +1,6 @@
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '0.7.1'
+__version__ = '0.8.0'
 
 import json
 from typing import Optional, TextIO, Type, TypeVar, Union
