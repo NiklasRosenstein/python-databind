6c090e8243a1178e7814b21ec520fd6164ef1ce0
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-06-03 02:20:15 +0200

    move files in databind.json

--- databind.json/src/databind/json/__init__.py
+++ databind.json/src/databind/json/__init__.py
@@ -1,3 +1,24 @@
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
 __version__ = '0.12.0'
+
+from databind.core.objectmapper import IModule, SimpleModule
+from .dataclass import DataclassModule
+from .datamodel import DatamodelModule
+
+__all__ = [
+  'DataclassModule',
+  'DatamodelModule',
+  'JsonModule',
+]
+
+
+class JsonModule(SimpleModule):
+  """
+  A composite of all modules for JSON de/serialization.
+  """
+
+  def __init__(self, name: str = None) -> None:
+    super().__init__(name)
+    self.add_module(DataclassModule())
+    self.add_module(DatamodelModule())
