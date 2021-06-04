3d0da62a3a596ab7ab232a55c3232cb5b66cf40d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-06-04 15:39:35 +0000

    update dependencies and test.py

--- databind.core/test.py
+++ databind.core/test.py
@@ -1,8 +1,9 @@
 import typing as t
 from dataclasses import dataclass, field
 from databind.core.annotations.alias import alias
+from databind.core.annotations.datefmt import datefmt
 from databind.core.annotations.unionclass import unionclass
-from databind.core.api import Context, DeserializerEnvironment
+from databind.core.api import Context
 from databind.core.location import Location
 from databind.core.objectmapper import ObjectMapper
 from databind.core.typehint import Concrete
@@ -22,6 +23,15 @@ class Student(Person):
     visits_courses = [alias('teaches-courses')]
 
 mapper = ObjectMapper(JsonModule())
-v = Student('John Doe', {'Physics', 'Chemistry'})
-print(mapper.deserialize(v, Student))
+
+import datetime
+
+val = mapper.deserialize('2021-05-01', datetime.datetime, annotations=[datefmt('%Y-%m-%d')])
+print(val, repr(val))
+
+val = mapper.serialize(val, datetime.datetime, annotations=[datefmt('%d-%m-%Y')])
+print(val, repr(val))
+
+#v = Student('John Doe', {'Physics', 'Chemistry'})
+#print(mapper.deserialize(v, Student))
 
