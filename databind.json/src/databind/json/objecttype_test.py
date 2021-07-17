e90af85e21efa17be53f91140f64ce942e8ca4be
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-07-17 04:03:39 +0200

    start with objecttype unittest

--- databind.json/src/databind/json/objecttype_test.py
+++ databind.json/src/databind/json/objecttype_test.py
@@ -1,10 +1,20 @@
 
 import dataclasses
+import pytest
 from databind.core.objectmapper import ObjectMapper
 from databind.json import JsonModule
 
 mapper = ObjectMapper.default(JsonModule())
 
 
-def test_unionclass_deserializer():
-  pass
+@pytest.mark.skip('not implemented')
+def test_object_deserializer():
+
+  @dataclasses.dataclass
+  class Person:
+    name: str
+    age: int = 22
+
+  assert mapper.serialize(Person('John'), Person) == {'name': 'John', 'age': 22}
+  assert mapper.deserialize({'name': 'John', 'age': 40}, Person) == Person('John', 40)
+  assert mapper.deserialize({'name': 'John'}, Person) == Person('John', 22)
