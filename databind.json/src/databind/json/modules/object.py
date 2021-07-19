63ea93a086ce9404d37b03aa1001ba1307a16be4
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-07-19 10:58:29 +0200

    implement ObjectTypeConverter

--- databind.json/src/databind/json/modules/object.py
+++ databind.json/src/databind/json/modules/object.py
@@ -7,6 +7,7 @@ schemas (see #databind.core.schema).
 import typing as t
 from databind.core.api import Context, ConverterNotFound, Direction, IConverter, Context
 from databind.core.objectmapper import Module
+from databind.core.schema import Field, Schema
 from databind.core.types import ObjectType, BaseType
 
 
@@ -20,6 +21,55 @@ class ObjectModule(Module):
 
 class ObjectTypeConverter(IConverter):
 
+
   def convert(self, ctx: Context) -> t.Any:
-    print('--> datamodel', ctx)
-    raise NotImplementedError  # TODO
+    assert isinstance(ctx.type, ObjectType)
+
+    if ctx.direction == Direction.serialize:
+      if not isinstance(ctx.value, ctx.type.schema.python_type):
+        raise ctx.type_error(expected=ctx.type.schema.python_type)
+
+      groups: t.Dict[str, t.Dict[str, t.Any]] = {}
+      for field in ctx.type.schema.fields.values():
+        if not field.flat: continue
+        value = getattr(ctx.value, field.name)
+        groups[field.name] = ctx.push(field.type, value, field.name, field).convert()
+
+      result: t.Dict[str, t.Any] = {}
+      for field in ctx.type.schema.flat_fields():
+        alias = (field.field.aliases or [field.name])[0]
+        if field.group == '$':
+          value = getattr(ctx.value, field.name)
+          result[alias] = ctx.push(field.field.type, value, field.name, field.field).convert()
+        else:
+          result[alias] = groups[field.group][alias]
+
+      # TODO (@NiklasRosenstein): Support flat MapType() field
+
+      return result
+
+    elif ctx.direction == Direction.deserialize:
+      if not isinstance(ctx.value, t.Mapping):
+        raise ctx.type_error(expected=t.Mapping)
+
+      groups: t.Dict[str, t.Dict[str, t.Any]] = {'$': {}}
+      used_keys: t.Set[str] = set()
+      for field in ctx.type.schema.flat_fields():
+        aliases = field.field.aliases or [field.name]
+        for alias in aliases:
+          if alias in ctx.value:
+            value = ctx.value[field.name]
+            groups.setdefault(field.group, {})[field.name] = \
+              ctx.push(field.field.type, value, field.name, field.field).convert()
+            used_keys.add(alias)
+
+      for group, values in groups.items():
+        if group == '$': continue
+        field = ctx.type.schema.fields[group]
+        groups['$'][group] = ctx.push(field.type, values, group, field).convert()
+
+      # TODO (@NiklasRosenstein): Support flat MapType() field
+
+      return ctx.type.schema.composer.compose(groups['$'])
+
+    assert False
--- databind.json/src/databind/json/modules/object_test.py
+++ databind.json/src/databind/json/modules/object_test.py
@@ -1,13 +1,13 @@
 
 import dataclasses
-import pytest
+import typing_extensions as te
+from databind.core import annotations as A
 from databind.core.objectmapper import ObjectMapper
 from databind.json import JsonModule
 
 mapper = ObjectMapper.default(JsonModule())
 
 
-@pytest.mark.skip('not implemented')
 def test_object_deserializer():
 
   @dataclasses.dataclass
@@ -18,3 +18,19 @@ def test_object_deserializer():
   assert mapper.serialize(Person('John'), Person) == {'name': 'John', 'age': 22}
   assert mapper.deserialize({'name': 'John', 'age': 40}, Person) == Person('John', 40)
   assert mapper.deserialize({'name': 'John'}, Person) == Person('John', 22)
+
+  @dataclasses.dataclass
+  class Nested:
+    a: str
+    b: Person
+
+  assert mapper.serialize(Nested('foo', Person('John')), Nested) == {'a': 'foo', 'b': {'name': 'John', 'age': 22}}
+  assert mapper.deserialize({'a': 'foo', 'b': {'name': 'John', 'age': 22}}, Nested) == Nested('foo', Person('John'))
+
+  @dataclasses.dataclass
+  class Flat:
+    a: str
+    b: te.Annotated[Person, A.fieldinfo(flat=True)]
+
+  assert mapper.serialize(Flat('foo', Person('John')), Flat) == {'a': 'foo', 'name': 'John', 'age': 22}
+  assert mapper.deserialize({'a': 'foo', 'name': 'John', 'age': 22}, Flat) == Flat('foo', Person('John'))
