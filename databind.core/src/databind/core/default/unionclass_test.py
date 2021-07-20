1b2e8e0c29e41bf1c829c876d77408d207a4459b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-07-20 22:46:50 +0200

    add unittest for unionclass in t.Annotated[]

--- databind.core/src/databind/core/default/unionclass_test.py
+++ databind.core/src/databind/core/default/unionclass_test.py
@@ -1,10 +1,12 @@
 
 import dataclasses
+import typing as t
+import typing_extensions as te
 
 from databind.core.annotations.unionclass import unionclass
 from databind.core.default.dataclass import DataclassAdapter
 from databind.core.objectmapper import ObjectMapper
-from databind.core.types import UnionType, from_typing
+from databind.core.types import ListType, UnionType, from_typing
 from databind.core.union import DynamicSubtypes
 from .unionclass import UnionclassAdapter
 
@@ -30,3 +32,14 @@ def test_unionclass_adapter():
 
   mapper = ObjectMapper(DataclassAdapter(), UnionclassAdapter())
   _check(mapper.adapt_type_hint(from_typing(MyUnionType)))
+
+
+def test_unionclass_annotated_adapter():
+  members = t.List[te.Annotated[t.Union[int, str], unionclass({
+    'int': int,
+    'str': str,
+  })]]
+  assert ObjectMapper.default().adapt_type_hint(from_typing(members)) == ListType(UnionType(DynamicSubtypes({
+    'int': int,
+    'str': str,
+  }), python_type=t.Union[int, str]))
