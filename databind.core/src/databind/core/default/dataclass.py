76321c7fb2a8c03577182434c847f3088208fa0b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-07-21 12:49:17 +0200

    update databind.core.default.dataclass

--- databind.core/src/databind/core/default/dataclass.py
+++ databind.core/src/databind/core/default/dataclass.py
@@ -5,7 +5,7 @@ interpreted as a datamodel.
 """
 
 import typing as t
-from dataclasses import dataclass, is_dataclass, Field as _DataclassField, MISSING as _MISSING
+from dataclasses import is_dataclass, fields as _get_fields, MISSING as _MISSING
 from databind.core.annotations import get_type_annotations
 from databind.core.annotations.alias import alias
 from databind.core.api import Context, ITypeHintAdapter
@@ -16,18 +16,13 @@ from nr import preconditions
 from nr.pylang.utils.singletons import NotSet
 
 
-def enumerate_fields(dataclass_type: t.Type) -> t.Iterator[_DataclassField]:
-  preconditions.check_argument(is_dataclass(dataclass_type), 'dataclass_type must be dataclass')
-  return iter(dataclass_type.__dataclass_fields__.values())
-
-
 def dataclass_to_schema(dataclass_type: t.Type, adapter: t.Optional[ITypeHintAdapter] = None) -> Schema:
   preconditions.check_instance_of(dataclass_type, type)
   adapter = adapter or ITypeHintAdapter.Noop()
   fields: t.Dict[str, Field] = {}
   annotations = t.get_type_hints(dataclass_type)
 
-  for field in enumerate_fields(dataclass_type):
+  for field in _get_fields(dataclass_type):
     if not field.init:
       # If we cannot initialize the field in the constructor, we should also
       # exclude it from the definition of the type for de-/serializing.
