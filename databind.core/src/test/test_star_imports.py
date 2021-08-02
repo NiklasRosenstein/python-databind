b0f354d949ec3f64d2d2c3a1393fdca0f3af368f
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-02 15:45:11 +0200

    update unittest

--- databind.core/src/test/test_star_imports.py
+++ databind.core/src/test/test_star_imports.py
@@ -2,5 +2,5 @@
 from databind.core.annotations import *
 from databind.core.mapper import *
 from databind.core.types import *
-from databind.core.dataclasses import *
+from databind.core.dataclasses import *  # type: ignore
 from databind.core import *
