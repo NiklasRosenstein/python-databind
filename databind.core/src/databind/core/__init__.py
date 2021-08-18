0dbd78b0c2d53ed58980b3746c677717a5c51913
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-18 17:08:13 +0200

    (databind) bump version to 1.1.6

--- databind.core/src/databind/core/__init__.py
+++ databind.core/src/databind/core/__init__.py
@@ -1,6 +1,6 @@
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '1.1.5'
+__version__ = '1.1.6'
 
 # Export public APIs
 
--- databind.json/setup.py
+++ databind.json/setup.py
@@ -35,7 +35,7 @@ else:
   long_description = None
 
 requirements = [
-  'databind.core >=1.1.5,<2.0.0',
+  'databind.core >=1.1.6,<2.0.0',
   'typing_extensions >=3.10.0,<4.0.0',
   'nr.parsing.date >=1.0.1,<2.0.0',
   'nr.preconditions >=0.0.4,<1.0.0',
@@ -43,7 +43,7 @@ requirements = [
 
 setuptools.setup(
   name = 'databind.json',
-  version = '1.1.5',
+  version = '1.1.6',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'De-/serialize Python dataclasses to or from JSON payloads. Compatible with Python 3.7 and newer.',
