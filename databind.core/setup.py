cf95c38ec0e385848f18965affba4dd3eaa5ac06
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-09-01 06:09:51 +0200

    (databind) bump version to 1.2.2

--- databind.core/setup.py
+++ databind.core/setup.py
@@ -51,7 +51,7 @@ extras_require['test'] = test_requirements
 
 setuptools.setup(
   name = 'databind.core',
-  version = '1.2.1',
+  version = '1.2.2',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'Databind is a library inspired by jackson-databind to de-/serialize Python dataclasses. Compatible with Python 3.7 and newer.',
