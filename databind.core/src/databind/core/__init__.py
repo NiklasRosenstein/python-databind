5bb0bfea18292d9485cadb7c5b8ce8b3a5c048a7
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-01-22 01:28:47 +0100

    (databind) bump version to 1.4.0

--- databind.core/src/databind/core/__init__.py
+++ databind.core/src/databind/core/__init__.py
@@ -1,6 +1,6 @@
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '1.3.2'
+__version__ = '1.4.0'
 
 # Export public APIs
 
--- databind/src/databind/__init__.py
+++ databind/src/databind/__init__.py
@@ -1,3 +1,3 @@
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)  # type: ignore
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '1.3.2'
+__version__ = '1.4.0'
