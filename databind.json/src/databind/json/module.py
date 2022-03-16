af81e0a20604051eb2b4a53fc4e437ac0fa43ea4
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-03-16 02:35:08 +0100

    update JsonModule

--- databind.json/src/databind/json/module.py
+++ databind.json/src/databind/json/module.py
@@ -11,11 +11,18 @@ class JsonModule(Module):
   use one of the convenience static methods #serializing() and #deserializing(). """
 
   def __init__(self, direction: Direction) -> None:
+    super().__init__(f'JSON ({direction.name.lower()}')
     self.direction = direction
 
-    from databind.json.converters import AnyConverter, PlainDatatypeConverter
+    from databind.json.converters import AnyConverter, DatetimeConverter, DecimalConverter, DurationConverter, \
+        EnumConverter, OptionalConverter, PlainDatatypeConverter
 
     self.register(AnyConverter())
+    self.register(DatetimeConverter(direction))
+    self.register(DecimalConverter(direction))
+    self.register(DurationConverter(direction))
+    self.register(EnumConverter(direction))
+    self.register(OptionalConverter())
     self.register(PlainDatatypeConverter(direction))
 
   @staticmethod
