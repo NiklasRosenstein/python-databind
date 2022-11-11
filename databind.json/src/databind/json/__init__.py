212cd472f92039bafd4177ce4b98b742eaba6c6b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-11-11 11:14:02 +0000

    release 4.1.1

--- databind.json/src/databind/json/__init__.py
+++ databind.json/src/databind/json/__init__.py
@@ -9,7 +9,7 @@ if t.TYPE_CHECKING:
     from databind.core.mapper import ObjectMapper
     from databind.core.settings import Setting, Settings
 
-__version__ = "4.1.0"
+__version__ = "4.1.1"
 
 JsonType = t.Union[
     None,
