12a910ec96e573d688763f17f2ac8c8d1d724b67
Niklas Rosenstein <rosensteinniklas@gmail.com>
2023-05-28 17:46:26 +0000

    fix unit test that fails in some python versions due to different class where Annotated is definef

--- databind.json/src/databind/json/tests/converters_test.py
+++ databind.json/src/databind/json/tests/converters_test.py
@@ -399,13 +399,12 @@ def test_deserialize_as() -> None:
         assert mapper.serialize(MyClass(A(1)), MyClass) == {"a": {"a": 1}}
     assert (
         str(excinfo.value)
-        == """expected converters_test.test_deserialize_as.<locals>.B, got converters_test.test_deserialize_as.\
+        == f"""expected converters_test.test_deserialize_as.<locals>.B, got converters_test.test_deserialize_as.\
 <locals>.A instead
 
 Trace:
     $: TypeHint(converters_test.test_deserialize_as.<locals>.MyClass)
-    .a: TypeHint(typing_extensions.Annotated[converters_test.test_deserialize_as.<locals>.A, DeserializeAs(type=<class \
-'converters_test.test_deserialize_as.<locals>.B'>, priority=<Priority.NORMAL: 1>)])"""
+    .a: TypeHint({te.Annotated[A, DeserializeAs(B)]})"""
     )
 
     assert mapper.serialize(MyClass(B(2)), MyClass) == {"a": {"a": 2}}
